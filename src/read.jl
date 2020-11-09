8610e4eb1ddfb3fba7c11f9b544baa924cbfed38
TheRoniOne <ronidani99@hotmail.com>
2020-11-08 22:17:24 -0500

    refactored read to use NamedTuple

--- src/read.jl
+++ src/read.jl
@@ -1,42 +1,44 @@
 using DataFrames: DataFrame, FileIO
 
 function readMDTable(file::String; header::Bool=true)::DataFrame
-    mdTable = Dict()
-    headersClean::Array = []
+    mdTable::NamedTuple = NamedTuple()
     skip::Bool = header
     for line in eachline(file)
         if header
-            parseHeader!(line, mdTable, headersClean)
+            mdTable = parseHeader(line)
             header = false
         elseif skip
             skip = false
         else
-            parseLine!(line, mdTable, headersClean)
+            parseLine!(line, mdTable)
         end
     end
     return DataFrame(mdTable)
 end
 
-function parseHeader!(line::String, mdTable::Dict, headersClean::Array)
-    headersDirty = split(line, "|")
+function parseHeader(line::String)
+    headersDirty = split(line, "|", keepempty=false)
+    values::Array = []
+    headersClean::Array = []
     for value in headersDirty
         key = lstrip(rstrip(value))
         if key != ""
-            mdTable[key] = []
+            push!(values, [])
             push!(headersClean, key)
         end
     end
+    return NamedTuple{(Symbol.(headersClean)...,)}(values)
 end
 
-function parseLine!(line::String, mdTable::Dict, headersClean::Array)
+function parseLine!(line::String, mdTable::NamedTuple)
     values = split(line, "|", keepempty=false)
-    if length(values) != length(headersClean)
+    if length(values) != length(mdTable)
         error("The number of values in a line must be the same as the number of headers of the file")
     end
 
     for i in 1:length(values)
         value = lstrip(rstrip(values[i]))
-        push!(mdTable[headersClean[i]], value)
+        push!(getindex(mdTable, i), value)
     end
 end
 
