7b0520d2f544b1a45bb678d1fd2dbd34481202d2
TheRoniOne <ronidani99@hotmail.com>
2020-11-08 23:02:49 -0500

    implemented write

--- src/write.jl
+++ src/write.jl
@@ -5,9 +5,28 @@ function writeMDTable(fileName::String, df)
     rows = Tables.rows(df)
     sch = Tables.schema(rows)
     names = Tables.columnnames(rows)
-    for row in rows
-        Tables.eachcolumn(sch, row) do val, i, nm
-            println(val, i, nm)
+    header = true
+    open(fileName, "w") do io
+        headers::String = ""
+        for i in 1:length(names)
+            if i != length(names)
+                headers = headers * "| $names[i] "
+            else
+                headers = headers * "| $names[i] " * "|"
+            end
+        end
+        write(io, headers)
+    end
+
+    open(fileName, "a") do io
+        for row in rows
+            line::String = ""
+            Tables.eachcolumn(sch, row) do val, i, nm
+                if i == 1
+                    line = line * "| $val "
+                end
+            end
+            write(io, line * "|")
         end
     end
 end
