545b69aaa43c54cda56f426c100eafd5c18f8bd4
TheRoniOne <ronidani99@hotmail.com>
2020-11-08 21:22:38 -0500

    added Tables to deps

--- src/write.jl
+++ src/write.jl
@@ -1,5 +1,13 @@
-using DataFrames: DataFrame, FileIO
+using DataFrames: DataFrame
+using Tables, FileIO
 
 function writeMDTable(fileName::String, df)
-    
+    rows = Tables.rows(df)
+    sch = Tables.schema(rows)
+    names = Tables.columnnames(rows)
+    for row in rows
+        Tables.eachcolumn(sch, row) do val, i, nm
+            println(val, i, nm)
+        end
+    end
 end
